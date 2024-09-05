d07aebabb33603032085f1e894ede66e18f1442f
Stefan Stretz <stefan.stretz@tib.eu>
2024-09-05 19:04:32 +0200

    test

--- components/global/FacetDrawer.vue
+++ components/global/FacetDrawer.vue
@@ -158,7 +158,7 @@
                   <input
                     class="ais-SearchBox-input appearance-none [color-scheme:light] dark:[color-scheme:dark] selection:text-zinc-700 group-data-[has-overlay]:selection:!text-transparent text-sm text-zinc-700 min-w-0 min-h-[1.5em] grow outline-none bg-transparent selection:bg-bali-hai-100 placeholder:!text-zinc-300 group-data-[disabled]:!cursor-not-allowed dark:placeholder:!text-zinc-200/50 dark:!text-zinc-300 border-none focus:ring-0 formkit-input !p-3 !rounded-3xl"
                     :placeholder="$t('search')"
-                    @input="searchForItems($event.currentTarget.value)"
+                    @input="searchForItems($event?.currentTarget?.value)"
                   >
                   <ul>
                     <li v-if="isFromSearch && !items.length">
--- content/iconcard.md
+++
@@ -1,7 +0,0 @@
-::icon-card
----
-icon: IconNuxt
-description: Harness the full power of Nuxt and the Nuxt ecosystem.
-title: Nuxt Architecture.
----
-::
